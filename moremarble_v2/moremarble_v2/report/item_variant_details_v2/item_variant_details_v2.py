2be3a28fb8c885864438b2b37089114b7abe07c3
UsamaNaveed9 <usamanaveed9263@gmail.com>
2022-04-11 14:58:10 +0000

    conver str into int

--- moremarble_v2/moremarble_v2/report/item_variant_details_v2/item_variant_details_v2.py
+++ moremarble_v2/moremarble_v2/report/item_variant_details_v2/item_variant_details_v2.py
@@ -12,10 +12,10 @@ def execute(filters=None):
 	data = get_data(filters.item)
 
 	for rec in data:
-		length = rec['length']
-		width = rec['width']
+		length = int(rec['length'])
+		width = int(rec['width'])
 		area = length*width
-		rec["stock_in_slab"] = rec['current_stock']/area
+		rec["stock_in_slab"] = int(rec['current_stock'])/area
 
 
 	return columns, data
